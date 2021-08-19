aa7a048e08b3ef78718ed4208ccea440bcec0efb
CastilloRodrig0 <rcastillo288@gmail.com>
2021-08-19 18:10:46 -0300

    Nuevos archivos

---
+++ Clases/script.js
@@ -0,0 +1,11 @@
+class Persona {
+    constructor(nombre,apellido,edad){
+        this.nombre=nombre;
+        this.apellido=apellido;
+        this.edad=edad;
+    }
+}
+
+let rodrigo = new Persona('Rodrigo','Castillo',24);
+
+console.log(rodrigo);
